38228c69c4807558560a4116811f9d0cc2109f34
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-17 09:15:39 +0530

    python

---
+++ warmup1/nearhundred.py
@@ -0,0 +1,10 @@
+def near_hundred(n):
+    if (n >=90 and n <= 110):
+        return True
+    elif (n >= 190 and n <=210):
+        return True
+    else:
+        return False
+        
+a = near_hundred(211)
+print (a)
